f650e78a5f4c9143ccb67e23cea52d3b65140d3a
kevinrichard31 <kevinrichard31@gmail.com>
2025-08-19 13:07:58 +0200

    Create offer route

---
+++ Controller/Adminhtml/Offer/Create.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Dnd\OfferManager\Controller\Adminhtml\Offer;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Phrase;
+
+class Create extends Action
+{
+    const ADMIN_RESOURCE = 'Dnd_OfferManager::offers';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    public function __construct(
+        Action\Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Execute action
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+    $resultPage = $this->resultPageFactory->create();
+    $resultPage->getConfig()->getTitle()->prepend(new Phrase('Add New Offer'));
+
+        return $resultPage;
+    }
+}
